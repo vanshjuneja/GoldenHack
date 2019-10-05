1c786ef8f6abaaf52ed5e849bbc732363932e20f
Bill Bai <billbai0102@gmail.com>
2019-10-05 17:01:38 -0400

    e

--- PythonScripts/TestMain.py
+++ PythonScripts/TestMain.py
@@ -1,4 +1,5 @@
 import CompareAssetsVsLiabilties as ca
+import AssetsVsAssets as aa
 
-
-c = ca.CompareAssetsVsLiabilities(assets=5, liabilties=10)
+#c = ca.CompareAssetsVsLiabilities(assets=5, liabilties=10)
+c = aa.AssetsVsAssets(30, 5, 3, 40, 3,5)
