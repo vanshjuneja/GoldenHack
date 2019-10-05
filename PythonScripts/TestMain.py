8f912e6b9e8416d3679efa7ed4319536143e6840
Bill Bai <billbai0102@gmail.com>
2019-10-05 17:20:12 -0400

    e

--- PythonScripts/TestMain.py
+++ PythonScripts/TestMain.py
@@ -1,5 +1,7 @@
 import CompareAssetsVsLiabilties as ca
 import AssetsVsAssets as aa
+import EquityVsEquity as ee
 
-#c = ca.CompareAssetsVsLiabilities(assets=5, liabilties=10)
-c = aa.AssetsVsAssets(30, 5, 3, 40, 3,5)
+# c1 = ca.CompareAssetsVsLiabilities(assets=5, liabilties=10)
+# c2 = aa.AssetsVsAssets(30, 5, 40, 3)
+c3 = ee.EquityVsEquity(15,12)
