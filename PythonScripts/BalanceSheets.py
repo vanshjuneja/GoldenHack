66ebfaf4d97a90c70f06481c9f7d4014e2cfede2
Bill Bai <billbai0102@gmail.com>
2019-10-05 22:25:01 -0400

    vc

--- PythonScripts/BalanceSheets.py
+++ PythonScripts/BalanceSheets.py
@@ -1,59 +1,120 @@
 import csv
 
-with open('Financial_Statements\BalanceSheet1.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    count = 0
+def oldBalanceSheet():
+    with open('Financial_Statements\BalanceSheet2.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
 
-    assets = open("assets.txt", 'w')
-    liab = open("liab.txt", 'w')
-    oe = open("oe.txt", 'w')
-    assetsVSLiab = open("assets_vs_liab.txt", 'w')
+        assets = open("assetsOld.txt", 'w')
+        liab = open("liabOld.txt", 'w')
+        oe = open("oeOld.txt", 'w')
+        assetsVSLiab = open("assets_vs_liabOld.txt", 'w')
 
-    asset_titles = []
-    asset_values = []
-    liab_titles = []
-    liab_values = []
-    oe_titles = []
-    oe_values = []
-    oe_total = 0
-    liab_total = 0
-    oe_total = 0
+        asset_titles = []
+        asset_values = []
+        liab_titles = []
+        liab_values = []
+        oe_titles = []
+        oe_values = []
+        oe_total = 0
+        liab_total = 0
+        oe_total = 0
 
-    for row in csv_reader:
-        if count > 4 and count < 17:
-            asset_titles.append(row[0])
-            asset_values.append(row[1])
-        elif count == 18:
-            asset_total = row[1]
-        elif count > 20 and count < 28:
-            liab_titles.append(row[0])
-            liab_values.append(row[1])
-        elif count == 28:
-            liab_total = row[1]
-        elif count == 33:
-            oe_total = row[1]
-        count += 1
+        for row in csv_reader:
+            if count > 4 and count < 17:
+                asset_titles.append(row[0])
+                asset_values.append(row[1])
+            elif count == 18:
+                asset_total = row[1]
+            elif count > 20 and count < 28:
+                liab_titles.append(row[0])
+                liab_values.append(row[1])
+            elif count == 29:
+                liab_total = row[1]
+            elif count == 34:
+                oe_total = row[1]
+            count += 1
 
-    for x in range(0,asset_values.__len__()):
-        print(asset_titles[x]+': '+asset_values[x])
-        assets.write(asset_titles[x]+'\n')
-        assets.write(asset_values[x]+'\n')
-    for x in range(0,liab_values.__len__()):
-        print(liab_titles[x]+': '+liab_values[x])
-        liab.write(liab_titles[x] + '\n')
-        liab.write(liab_values[x] + '\n')
-    print("Assets Total: ", asset_total)
-    print("Liabilities Total: ", liab_total)
-    print("OE Total: ", oe_total)
+        for x in range(0,asset_values.__len__()):
+            print(asset_titles[x]+': '+asset_values[x])
+            assets.write(asset_titles[x]+'\n')
+            assets.write(asset_values[x]+'\n')
+        for x in range(0,liab_values.__len__()):
+            print(liab_titles[x]+': '+liab_values[x])
+            liab.write(liab_titles[x] + '\n')
+            liab.write(liab_values[x] + '\n')
+        print("Assets Total: ", asset_total)
+        print("Liabilities Total: ", liab_total)
+        print("OE Total: ", oe_total)
 
-    assetsVSLiab.write('Assets' + '\n')
-    assetsVSLiab.write(asset_total + '\n')
-    assetsVSLiab.write('Liabilities' + '\n')
-    assetsVSLiab.write(liab_total)
+        assetsVSLiab.write('Assets' + '\n')
+        assetsVSLiab.write(asset_total + '\n')
+        assetsVSLiab.write('Liabilities' + '\n')
+        assetsVSLiab.write(liab_total)
 
-    oe.write('Owner\'s Equity' + '\n' + oe_total)
+        oe.write('Owner\'s Equity' + '\n' + oe_total)
 
-    assets.close()
-    liab.close()
-    oe.close()
-    assetsVSLiab.close()
+        assets.close()
+        liab.close()
+        oe.close()
+        assetsVSLiab.close()
+
+
+def newBalanceSheet():
+    with open('Financial_Statements\BalanceSheet1.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        count = 0
+
+        assets = open("assets.txt", 'w')
+        liab = open("liab.txt", 'w')
+        oe = open("oe.txt", 'w')
+        assetsVSLiab = open("assets_vs_liab.txt", 'w')
+
+        asset_titles = []
+        asset_values = []
+        liab_titles = []
+        liab_values = []
+        oe_titles = []
+        oe_values = []
+        oe_total = 0
+        liab_total = 0
+        oe_total = 0
+
+        for row in csv_reader:
+            if count > 4 and count < 17:
+                asset_titles.append(row[0])
+                asset_values.append(row[1])
+            elif count == 18:
+                asset_total = row[1]
+            elif count > 20 and count < 28:
+                liab_titles.append(row[0])
+                liab_values.append(row[1])
+            elif count == 29:
+                liab_total = row[1]
+            elif count == 34:
+                oe_total = row[1]
+            count += 1
+
+        for x in range(0, asset_values.__len__()):
+            print(asset_titles[x] + ': ' + asset_values[x])
+            assets.write(asset_titles[x] + '\n')
+            assets.write(asset_values[x] + '\n')
+        for x in range(0, liab_values.__len__()):
+            print(liab_titles[x] + ': ' + liab_values[x])
+            liab.write(liab_titles[x] + '\n')
+            liab.write(liab_values[x] + '\n')
+        print("New Assets Total: ", asset_total)
+        print("New Liabilities Total: ", liab_total)
+        print("New OE Total: ", oe_total)
+
+        assetsVSLiab.write('Assets' + '\n')
+        assetsVSLiab.write(asset_total + '\n')
+        assetsVSLiab.write('Liabilities' + '\n')
+        assetsVSLiab.write(liab_total)
+
+        oe.write('Owner\'s Equity' + '\n' + oe_total)
+
+        assets.close()
+        liab.close()
+        oe.close()
+        assetsVSLiab.close()
--- PythonScripts/TestMain.py
+++ PythonScripts/TestMain.py
@@ -7,6 +7,100 @@ import RevenueVsExpenses as re
 import RevenueVsRevenue as rr
 import BalanceSheets as bs
 
+# INITIALIZATION
+bs.oldBalanceSheet()
+bs.newBalanceSheet()
+
+########## VARIABLES ##########
+old_assets = open("assetsOld.txt", 'r')
+old_liab = open("liabOld.txt", 'r')
+old_oe = open("oeOld.txt", 'r')
+old_assetsVSLiab = open("assets_vs_liabOld.txt", 'r')
+assets = open("assets.txt", 'r')
+liab = open("liab.txt", 'r')
+oe = open("oe.txt", 'r')
+assetsVSLiab = open("assets_vs_liab.txt", 'r')
+
+old_a_vals = []
+old_a_titles = []
+
+old_L_vals = []
+old_L_titles = []
+
+old_oe_total = 0
+
+old_assetsTotal = 0
+old_liabTotal = 0
+
+a_vals = []
+a_titles = []
+
+L_vals = []
+L_titles = []
+
+oe_total = 0
+assetsTotal = 0
+liabTotal = 0
+
+
+########## ######### ##########
+
+
+# TextExtractor Methods
+def getVals():
+    global oe_total
+    global assetsTotal
+    global liabTotal
+    global old_oe_total
+    global old_assetsTotal
+    global old_liabTotal
+    line = old_assets.readline().replace('\n', '')
+
+    while line:
+        old_a_titles.append(line)
+        line = old_assets.readline().replace('\n', '').replace(',', '')
+        old_a_vals.append(int(line))
+        line = old_assets.readline().replace('\n', '').replace(',', '')
+
+    line = old_liab.readline().replace('\n', '')
+
+    while line:
+        old_L_titles.append(line)
+        line = old_liab.readline().replace('\n', '').replace(',', '')
+        old_L_vals.append(int(line))
+        line = old_liab.readline().replace('\n', '').replace(',', '')
+
+    old_oe.readline()
+    old_oe_total = old_oe.readline().replace('\n', '')
+
+    line = old_assetsVSLiab.readline()
+    old_assetsTotal = old_assetsVSLiab.readline().replace('\n', '').replace(',', '')
+    old_assetsVSLiab.readline()
+    old_liabTotal = old_assetsVSLiab.readline().replace('\n', '').replace(',', '')
+
+    line = assets.readline().replace('\n', '')
+
+    while line:
+        a_titles.append(line)
+        line = assets.readline().replace('\n', '').replace(',', '')
+        a_vals.append(int(line))
+        line = assets.readline().replace('\n', '').replace(',', '')
+
+    line = liab.readline().replace('\n', '')
+
+    while line:
+        L_titles.append(line)
+        line = liab.readline().replace('\n', '').replace(',', '')
+        L_vals.append(int(line))
+        line = liab.readline().replace('\n', '').replace(',', '')
+
+    oe.readline()
+    oe_total = int(oe.readline().replace('\n', '').replace(',', ''))
+
+    assetsVSLiab.readline()
+    assetsTotal = int(assetsVSLiab.readline().replace('\n', '').replace(',', ''))
+    assetsVSLiab.readline()
+    liabTotal = int(assetsVSLiab.readline().replace('\n', '').replace(',', ''))
 
 
 # c1 = ca.CompareAssetsVsLiabilities(assets=5, liabilties=10)
@@ -21,4 +115,30 @@ import BalanceSheets as bs
 #      'beill9', 'birll9'],
 #     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 # c6 = re.RevenueVsExpenses(100, 5)
-#c7 = rr.RevenueVsRevenue(1003.24,2382.54, 1200, 2000)
+# c7 = rr.RevenueVsRevenue(1003.24,2382.54, 1200, 2000)
+
+getVals()
+
+print('______________________________')
+print('______________________________')
+print('______________________________')
+
+print(a_titles)
+print(a_vals)
+print(L_titles)
+print(L_vals)
+print(oe_total)
+print(assetsTotal)
+print(liabTotal)
+
+print('______________________________')
+print('______________________________')
+print('______________________________')
+
+print(old_a_titles)
+print(old_a_vals)
+print(old_L_titles)
+print(old_L_vals)
+print(old_oe_total)
+print(old_assetsTotal)
+print(old_liabTotal)
