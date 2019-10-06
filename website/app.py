3cc111104a02cf3fee8bc9ad92b5c14adcb9690f
Vansh Juneja <vanshjuneja03@gmail.com>
2019-10-06 07:41:44 -0400

    efs

--- website/app.py
+++ website/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -10,9 +11,14 @@ def main():
 def upload():
     return render_template('upload.html')
 
-@app.route("/Main")
+@app.route("/pypy")
 def gopy():
     import Main.py as pkg
+
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1", port=8000)
