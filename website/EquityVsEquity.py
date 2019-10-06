c79bae92d490853e3ee3085834ead592d38e83a8
Bill Bai <billbai0102@gmail.com>
2019-10-06 07:40:51 -0400

    Formatting

--- website/EquityVsEquity.py
+++ website/EquityVsEquity.py
@@ -9,7 +9,7 @@ class EquityVsEquity(object):
         fig.add_trace(go.Bar(
             y=[name1],
             x=[equities1],
-            name='Previous Year Equities',
+            name='2018<br>Owners<br>Equity',
             orientation='h',
             marker=dict(
                 color='#5DADE2',
@@ -18,11 +18,11 @@ class EquityVsEquity(object):
         fig.add_trace(go.Bar(
             y=['Current Year Equities'],
             x=[equities2],
-            name='Current Year Equities',
+            name='2019<br>Owners<br>Equity',
             orientation='h',
             marker=dict(
                 color='#FFA500',
             )
         ))
-
+        fig.update_layout(showlegend=False)
         pyo.write_html(fig, file='static/EquitiesVsEquities.html', auto_open=False)
